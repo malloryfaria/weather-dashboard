6b3deea0291b450495310abba621428f744e8154
Mallory <mallory.faria@gmail.com>
2021-01-17 02:29:38 -0500

    pushing latest updates - script file left to work on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,119 +1,55 @@
-// Declared variables
-var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-console.log(searchHistory);
-var form = document.querySelector("input-form");
-var cityInput = document.querySelector("city-input");
-var inputList = document.querySelector("input-list");
-var formHistory = document.querySelector("history");
-var cityName = document.getElementById("#city-name");
-var currentWeatherImg = document.getElementById("#current-weather");
-var humidity = document.getElementById("#humidity");
-var windSpeed = document.getElementById("#wind-speed");
-var uvIndex = document.getElementById("#UV-index");
+ $(document).ready(function () {
 
-// Open Weather API Key
+        // Declare the variables
 
-var apiKey = "a0cbd2da9176d7aecdf42af83bd9df06";
-
-var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=metric&appid=" + apiKey;
-
+        var cityInput = document.getElementById("#city-input");
+        var cityDisplayName = document.getElementById("#city-name");
+        var currentWeather = document.getElementById("#current-weather");
+        var currentForecast = document.getElementById("#today-forecast");
+        var currentDate = document.getElementById("#current-date");
+        var currentTemp = document.getElementById("#current-temperature");
+        var currentHumidity = document.getElementById("#current-humidity");
+        var currentWind = document.getElementById("#current-wind");
+        var currentUV = document.getElementById("#current-uv-index");
+        var historyForm = document.getElementById("#history");
+        var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+        var APIKey = "a0cbd2da9176d7aecdf42af83bd9df06";
+        
 
 
-$("#search-button").on("click", function(){
-    console.log("Search button clicked");
-    fetch(apiURL)
-  .then(response => response.json())
-  .then(data => console.log(data));
-})
+        // On page load, display search history
+        console.log(searchHistory);
 
-var formSubmitHandler = function(event) {
+        //  When search button is clicked, read the city name typed by the user
+        $("#search-button").on("click", function() {
+            event.preventDefault();
+            var searchInput = $("#city-input").val();
+            console.log(searchInput);
+            getWeather(searchInput);
 
-    // prevent page from refreshing
-    event.preventDefault();
-  
-    // get value from input element
-    var city = cityInput.value.trim();
 
-    // format the API URL
-    //  The format for the query to send to open weather map
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
-  
-    if (city) {
-      getForecastData(city);
-  
-      // clear old content
-      cityInput.textContent = '';
-      cityInput.value = '';
-    } else {
-      alert('Please enter a city name');
-    }
-  };
-  
-  var getForecastData = function(city, apiURL) {
-  
-    // make a get request to url
-    fetch(apiURL)
-      .then(function(response) {
-        // request was successful
-        if (response.ok) {
-          console.log(response);
-          response.json().then(function(data) {
-            console.log(data);
-            displayRepos(data, city);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function(error) {
-        alert('Unable to connect to weather provider');
-      });
-  };
-  
-//   var displayForecast = function(repos, searchTerm) {
-//     // check if api returned any repos
-//     if (repos.length === 0) {
-//       repoContainerEl.textContent = 'No repositories found.';
-//       return;
-//     }
-  
-//     repoSearchTerm.textContent = searchTerm;
-  
-//     // loop over repos
-//     for (var i = 0; i < repos.length; i++) {
-//       // format repo name
-//       var repoName = repos[i].owner.login + '/' + repos[i].name;
-  
-//       // create a container for each repo
-//       var repoEl = document.createElement('div');
-//       repoEl.classList = 'list-item flex-row justify-space-between align-center';
-  
-//       // create a span element to hold repository name
-//       var titleEl = document.createElement('span');
-//       titleEl.textContent = repoName;
-  
-//       // append to container
-//       repoEl.appendChild(titleEl);
-  
-//       // create a status element
-//       var statusEl = document.createElement('span');
-//       statusEl.classList = 'flex-row align-center';
-  
-//       // check if current repo has issues or not
-//       if (repos[i].open_issues_count > 0) {
-//         statusEl.innerHTML =
-//           "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-//       } else {
-//         statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-//       }
-  
-//       // append to container
-//       repoEl.appendChild(statusEl);
-  
-//       // append container to the dom
-//       repoContainerEl.appendChild(repoEl);
-//     }
-//   };
-  
-//   // add event listeners to forms
-//   userFormEl.addEventListener('submit', formSubmitHandler);
+        }); 
+
+        function getWeather(searchInput) {
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&unit=metric&appid=" + APIKey;
+            //  Using saved city name, execute a current condition get request from open weather map api
+                fetch(queryURL)
+                .then(function(response){
+                    console.log(response);
+                    // Set the date to today's date
+                    currentDate = moment();
+                    // Set the city to the searched city & the date
+                    cityDisplayName.innerHTML = response.data.name + currentDate;
+
+                    // Parse the response to be able to display the data
+
+                })
+        };
+
+
+});
+
+
+
+
+
